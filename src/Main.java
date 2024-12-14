511e14e843c6728e13b83093214367afd0b9d2fe
Rui Kawakami <e245706@ie.u-ryukyu.ac.jp>
2024-12-14 16:24:44 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         String str = null;
 
+        try {
         System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生しました。エラー内容: " + e.getMessage());
+        }
     }
 }
